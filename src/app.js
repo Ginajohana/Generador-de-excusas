6a277f732d8fd863722c4c4fcdb58486e4cab59b
Ginajohana <124780437+Ginajohana@users.noreply.github.com>
2023-04-18 11:14:46 +0000

    Se genera GENERATE EXCUSE

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+  console.log("hello rigo from console" + "generateExcuse");
+};
+let generateExcuse = () => {
+  let who = ["Mr. Burns", "Barth", "Marge", "Moe"];
+  let action = ["perdió", "rompió", "se llevó", "cogió"];
+  let what = ["la cerveza", "el coche", "las llaves", "el zapato"];
+  let when = [
+    "al salir de bar",
+    "mientras caminaba",
+    "durante la comida",
+    "cuando estaba fuera"
+  ];
+
+  let whoindex = Math.floor(Math.random() * who.length);
+  let actionindex = Math.floor(Math.random() * action.length);
+  let whatindex = Math.floor(Math.random() * what.length);
+  let whenindex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoindex] +
+    " " +
+    action[actionindex] +
+    " " +
+    what[whatindex] +
+    " " +
+    when[whenindex]
+  );
 };
